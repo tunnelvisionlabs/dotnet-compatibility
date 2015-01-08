7ed283938a41df12ce5806b87df061d6b293c292
Sam Harwell <sam.harwell@rackspace.com>
2015-01-08 11:14:03 -0600

    Add tests for AbstractMustNotBeAddedToType

---
+++ CompatibilityCheckerTests/TestTypeAnalysis.cs
@@ -0,0 +1,101 @@
+﻿namespace CompatibilityCheckerTests
+{
+    using System.Collections.ObjectModel;
+    using System.Reflection;
+    using System.Reflection.Emit;
+    using CompatibilityChecker;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TestTypeAnalysis
+    {
+        [TestMethod]
+        public void TestAbstractMustNotBeAddedToType_PassUnchanged()
+        {
+            AssemblyName assemblyName = new AssemblyName("Test.Assembly");
+            AssemblyBuilder referenceAssemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Save);
+            ModuleBuilder referenceModuleBuilder = referenceAssemblyBuilder.DefineDynamicModule(assemblyName.Name, "Test.Assembly.dll");
+            TypeBuilder referenceTypeBuilder = referenceModuleBuilder.DefineType("MyType", TypeAttributes.Class | TypeAttributes.Public, typeof(object));
+            referenceTypeBuilder.DefineDefaultConstructor(MethodAttributes.Family);
+            referenceTypeBuilder.CreateType();
+            referenceAssemblyBuilder.Save("Test.Assembly.dll", PortableExecutableKinds.ILOnly, ImageFileMachine.AMD64);
+
+            AssemblyBuilder newAssemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Save);
+            ModuleBuilder newModuleBuilder = newAssemblyBuilder.DefineDynamicModule(assemblyName.Name, "Test.Assembly.V2.dll");
+            TypeBuilder newTypeBuilder = newModuleBuilder.DefineType("MyType", TypeAttributes.Class | TypeAttributes.Public, typeof(object));
+            newTypeBuilder.DefineDefaultConstructor(MethodAttributes.Family);
+            newTypeBuilder.CreateType();
+            newAssemblyBuilder.Save("Test.Assembly.V2.dll", PortableExecutableKinds.ILOnly, ImageFileMachine.AMD64);
+
+            ReadOnlyCollection<Message> messages = TestUtility.AnalyzeAssemblies("Test.Assembly.dll", "Test.Assembly.V2.dll");
+            Assert.AreEqual(0, messages.Count);
+        }
+
+        [TestMethod]
+        public void TestAbstractMustNotBeAddedToType_PassRemoved()
+        {
+            AssemblyName assemblyName = new AssemblyName("Test.Assembly");
+            AssemblyBuilder referenceAssemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Save);
+            ModuleBuilder referenceModuleBuilder = referenceAssemblyBuilder.DefineDynamicModule(assemblyName.Name, "Test.Assembly.dll");
+            TypeBuilder referenceTypeBuilder = referenceModuleBuilder.DefineType("MyType", TypeAttributes.Class | TypeAttributes.Public | TypeAttributes.Abstract, typeof(object));
+            referenceTypeBuilder.DefineDefaultConstructor(MethodAttributes.Family);
+            referenceTypeBuilder.CreateType();
+            referenceAssemblyBuilder.Save("Test.Assembly.dll", PortableExecutableKinds.ILOnly, ImageFileMachine.AMD64);
+
+            AssemblyBuilder newAssemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Save);
+            ModuleBuilder newModuleBuilder = newAssemblyBuilder.DefineDynamicModule(assemblyName.Name, "Test.Assembly.V2.dll");
+            TypeBuilder newTypeBuilder = newModuleBuilder.DefineType("MyType", TypeAttributes.Class | TypeAttributes.Public, typeof(object));
+            newTypeBuilder.DefineDefaultConstructor(MethodAttributes.Family);
+            newTypeBuilder.CreateType();
+            newAssemblyBuilder.Save("Test.Assembly.V2.dll", PortableExecutableKinds.ILOnly, ImageFileMachine.AMD64);
+
+            ReadOnlyCollection<Message> messages = TestUtility.AnalyzeAssemblies("Test.Assembly.dll", "Test.Assembly.V2.dll");
+            Assert.AreEqual(0, messages.Count);
+        }
+
+        [TestMethod]
+        public void TestAbstractMustNotBeAddedToType_PassNotPublic()
+        {
+            AssemblyName assemblyName = new AssemblyName("Test.Assembly");
+            AssemblyBuilder referenceAssemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Save);
+            ModuleBuilder referenceModuleBuilder = referenceAssemblyBuilder.DefineDynamicModule(assemblyName.Name, "Test.Assembly.dll");
+            TypeBuilder referenceTypeBuilder = referenceModuleBuilder.DefineType("MyType", TypeAttributes.Class | TypeAttributes.NotPublic, typeof(object));
+            referenceTypeBuilder.DefineDefaultConstructor(MethodAttributes.Family);
+            referenceTypeBuilder.CreateType();
+            referenceAssemblyBuilder.Save("Test.Assembly.dll", PortableExecutableKinds.ILOnly, ImageFileMachine.AMD64);
+
+            AssemblyBuilder newAssemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Save);
+            ModuleBuilder newModuleBuilder = newAssemblyBuilder.DefineDynamicModule(assemblyName.Name, "Test.Assembly.V2.dll");
+            TypeBuilder newTypeBuilder = newModuleBuilder.DefineType("MyType", TypeAttributes.Class | TypeAttributes.NotPublic | TypeAttributes.Abstract, typeof(object));
+            newTypeBuilder.DefineDefaultConstructor(MethodAttributes.Family);
+            newTypeBuilder.CreateType();
+            newAssemblyBuilder.Save("Test.Assembly.V2.dll", PortableExecutableKinds.ILOnly, ImageFileMachine.AMD64);
+
+            ReadOnlyCollection<Message> messages = TestUtility.AnalyzeAssemblies("Test.Assembly.dll", "Test.Assembly.V2.dll");
+            Assert.AreEqual(0, messages.Count);
+        }
+
+        [TestMethod]
+        public void TestAbstractMustNotBeAddedToType_Fail()
+        {
+            AssemblyName assemblyName = new AssemblyName("Test.Assembly");
+            AssemblyBuilder referenceAssemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Save);
+            ModuleBuilder referenceModuleBuilder = referenceAssemblyBuilder.DefineDynamicModule(assemblyName.Name, "Test.Assembly.dll");
+            TypeBuilder referenceTypeBuilder = referenceModuleBuilder.DefineType("MyType", TypeAttributes.Class | TypeAttributes.Public, typeof(object));
+            referenceTypeBuilder.DefineDefaultConstructor(MethodAttributes.Family);
+            referenceTypeBuilder.CreateType();
+            referenceAssemblyBuilder.Save("Test.Assembly.dll", PortableExecutableKinds.ILOnly, ImageFileMachine.AMD64);
+
+            AssemblyBuilder newAssemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Save);
+            ModuleBuilder newModuleBuilder = newAssemblyBuilder.DefineDynamicModule(assemblyName.Name, "Test.Assembly.V2.dll");
+            TypeBuilder newTypeBuilder = newModuleBuilder.DefineType("MyType", TypeAttributes.Class | TypeAttributes.Public | TypeAttributes.Abstract, typeof(object));
+            newTypeBuilder.DefineDefaultConstructor(MethodAttributes.Family);
+            newTypeBuilder.CreateType();
+            newAssemblyBuilder.Save("Test.Assembly.V2.dll", PortableExecutableKinds.ILOnly, ImageFileMachine.AMD64);
+
+            ReadOnlyCollection<Message> messages = TestUtility.AnalyzeAssemblies("Test.Assembly.dll", "Test.Assembly.V2.dll");
+            Assert.AreEqual(1, messages.Count);
+            Assert.AreEqual("Error AbstractMustNotBeAddedToType: The 'abstract' modifier cannot be added to a type.", messages[0].ToString());
+        }
+    }
+}
