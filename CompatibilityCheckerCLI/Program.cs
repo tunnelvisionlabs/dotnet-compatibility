0eaa584f4136e06f4d9ba9d6a706fc362c56badd
Erwin de Haan <EraYaN@users.noreply.github.com>
2019-02-05 17:00:33 +0100

    Added .NETFramework project

---
+++ CompatibilityCheckerCLI/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+using CompatibilityChecker;
+
+namespace CompatibilityCheckerCLI
+{
+    using File = System.IO.File;
+    using FileInfo = System.IO.FileInfo;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: reference.dll new.dll");
+                Environment.ExitCode = 1;
+            }
+            else
+            {
+                FileInfo referenceFile = new FileInfo(args[0]);
+                FileInfo newFile = new FileInfo(args[1]);
+                if (referenceFile.Exists && newFile.Exists)
+                {
+                    using (PEReader referenceAssembly = new PEReader(File.OpenRead(referenceFile.FullName)))
+                    {
+                        using (PEReader newAssembly = new PEReader(File.OpenRead(newFile.FullName)))
+                        {
+                            Analyzer analyzer = new Analyzer(referenceAssembly, newAssembly, null);
+                            analyzer.Run();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
